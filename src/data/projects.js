ec16d6febc9065c8533e2fb6c49bdfc1f489a00c
Jeric1231 <153045584+Jeric1231@users.noreply.github.com>
2025-11-29 00:02:08 +0800

    Fix Image Not showing

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,7 @@ export const projects = [
     id: 'p1',
     title: 'Gradify',
     desc: 'Gradify is an innovative web-based academic performance tracking system designed to streamline grade management for teachers and provide students with real-time feedback and insights.',
-    image: 'src/assets/gradify.jpg',
+    image: '/assets/gradify.jpg',
     tech: ['ReactJs','Java','Spring Boot','PostgreSQL', 'GitHub Actions', 'Postman'],
     live: 'https://gradify-three.vercel.app/',
     repo: ''
@@ -12,7 +12,7 @@ export const projects = [
     id: 'p2',
     title: 'ProjectSync',
     desc: 'ProjectSync is a project management platform that streamlines team collaboration and project execution. It offers tools for planning and scheduling projects, managing tasks, real-time collaboration, user authentication, reporting and analytics, and document management, helping teams stay organized and track progress efficiently from start to finish.',
-    image: 'src/assets/projectsync.jpg',
+    image: '/assets/projectsync.jpg',
     tech: ['Springboot','ReactJs','Java','Postman','MySQL Workbench'],
     live: '',
     repo: 'https://github.com/Ban-Coca/IT342_ProjectSync'
@@ -21,7 +21,7 @@ export const projects = [
     id: 'p3',
     title: 'Creative Clarity',
     desc: 'Creative Clarity is an academic organizer that helps users manage tasks, track courses, and monitor academic progress. It features secure user authentication, to-do lists with due dates, progress tracking, grade reporting with dashboards, and course-specific notes. The system also supports uploading and archiving images and searching for stored files, making academic organization easier and more efficient.',
-    image: 'src/assets/creativeclarity.jpg',
+    image: '/assets/creativeclarity.jpg',
     tech: ['Springboot','ReactJs','Java','Postman', 'MySQL Workbench'],
     live: '',
     repo: 'https://github.com/Ban-Coca/CreativeClarity'
