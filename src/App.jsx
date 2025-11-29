30c229ed9ab36fa3a958c2fbeb7d4de654606aec
Jeric1231 <153045584+Jeric1231@users.noreply.github.com>
2025-11-29 13:08:41 +0800

    Fixed run error of App.jsx because of import contact

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Nav from './components/Nav'
 import Hero from './components/Hero'
 import About from './components/About'
 import Projects from './components/Projects'
-import Contact from './components/contact'
+import Contact from './components/Contact'
 import Footer from './components/Footer'
 import AnimatedBackground from './components/AnimatedBackground' 
 
